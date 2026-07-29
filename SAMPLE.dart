dc54a5635b8df5e7fcdd86022d4043846b07ee2b
kulkarnivarshini850 <kulkarnivarshini850@gmail.com>
2026-07-28 22:01:04 -0700

    Update SAMPLE.dart

--- SAMPLE.dart
+++ SAMPLE.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
-void main() => runApp(const MyApp());
-Widget {
-  MyApp(});
- @override
-     title: 'Hello Flutter',
-     theme: ThemeData(primarySwatch: Colors.blue),
-     home: const Scaffold(
-       appBar: AppBar(title: Text('Home Page')),
-       body: Center(child: Text('Welcome to Flutter!')),
-     ),
-   );
- }
-}
+
